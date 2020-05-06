7a36d12be1b576ae1e0aa9e7b16e064089be0a17
Solitario <hijane.m.h@gmail.com>
2020-05-06 11:38:26 +0000

    dev fonctionalité A B C

---
+++ src/main/java/com/m2t/gitformaion/MyClass.java
@@ -0,0 +1,18 @@
+package com.m2t.gitformaion;
+
+/**
+ * Created by Solitario on 5/6/2020.
+ */
+public class MyClass {
+    public void fonctionalite_A(){
+
+    }
+
+    public void fonctionalite_B(){
+
+    }
+
+    public void fonctionalite_C(){
+
+    }
+}
